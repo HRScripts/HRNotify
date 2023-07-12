ef7a10069dc3bb719fbf6fcf73fa3dff6eefb123
HRScripts <138999193+HRScripts@users.noreply.github.com>
2023-07-12 21:46:20 +0300

    HRNotify

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'HRNotify'
+author 'HRScripts | hristiyan_raykov#0'
+description 'Notification resource for HR scripts'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_script 'client.lua'
+
+files {
+    'html/*.*'
+}
+
+lua54 'yes'
