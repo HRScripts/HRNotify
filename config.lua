f614451824a38ea67e6270ee0ae303e7b9ef0d98
HRScripts <138999193+HRScripts@users.noreply.github.com>
2023-10-26 19:57:21 +0300

    HRNotify V2

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.EnableTestCmds = true
+
+-- Add default notification location setting
+Config.DefaultNotificationPosition = 'top-right' -- avaible positions- top-right - center-right - bottom-right - frombelow-right - top-left - left-center - frombelow-left
